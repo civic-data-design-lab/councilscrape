5c223caaf2c1e921b65dbcd04c6b2b9b8b8d6348
adamjanicki2 <45052692+adamjanicki2@users.noreply.github.com>
2020-07-13 12:33:34 -0400

    Update topic_modeling.py

--- topic_modeling.py
+++ topic_modeling.py
@@ -3,18 +3,18 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 
 no_features = 1000
-no_topics = 20
-no_top_words = 10
+no_topics = 2
+no_top_words = 8
 
-def setup():
+MONTH_DICT = {1: 'Jan.', 2: 'Feb.', 3: 'Mar.', 4: 'Apr.', 5: 'May', 6: 'Jun.', 7: 'Jul.', 8: 'Aug.', 9: 'Sep.', 10: 'Oct.', 11: 'Nov.', 12: 'Dec.'}
+
+def setup(fname):
     """
     This function is specific to me because I'm loading in files in this directory
     """
-    articles = []
-    for i in range(2013, 2021):
-        with open(str(i)+'_Articles.csv') as f:
-            articles += [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
-    return articles
+    with open(fname) as f:
+        return [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+    
 
 def create_dataset(articles):
     """
@@ -57,7 +57,6 @@ def display_topics(model, feature_names, no_top_words):
 def write_results_to_csv(topic_text, filename):
     """
     parameters: the output from displat_topics, intended filename
-
     Writes the topic data to a csv file
     """
     with open(filename, 'w') as f:
@@ -65,16 +64,23 @@ def write_results_to_csv(topic_text, filename):
         wr.writerow(('Topic #:', 'topic data:'))
         for topic in topic_text:
             wr.writerow(topic)
+        print('successfully written to file: '+filename)
 
 if __name__ == '__main__':
     #Tests:
-    articles = setup()
-    documents = create_dataset(articles)
-    lda, tf_feature_names = run_lda(documents)
-    nmf, tfidf_feature_names = run_nmf(documents)
-    nmf_topics = display_topics(nmf, tfidf_feature_names, no_top_words)
-    lda_topics = display_topics(lda, tf_feature_names, no_top_words)
-    write_results_to_csv(nmf_topics, 'NMF_Results.csv')
-    write_results_to_csv(lda_topics, 'LDA_Results.csv')
-    
-    
+    # for y in range(2020, 2021):
+    #     year = str(y)
+    #     all_topics = []
+    #     for i in range(1, 8):
+    #         if i < 10:
+    #             month = '0'+str(i)
+    #         else:
+    #             month = str(i)
+    #         articles = setup(month+'_'+year+'_Articles.csv')
+    #         documents = create_dataset(articles)
+    #         lda, tf_feature_names = run_lda(documents)
+    #         lda_topics = display_topics(lda, tf_feature_names, no_top_words)
+    #         all_topics+= [(MONTH_DICT[i],)]
+    #         all_topics+=lda_topics
+    #     write_results_to_csv(all_topics, year+'_LDA_Results.csv')
+    pass
