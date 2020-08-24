27b43f1a572b319ef769c3eb5e6653b807b70cff
adamjanicki2 <45052692+adamjanicki2@users.noreply.github.com>
2020-08-24 12:04:18 -0400

    Update topic_assignment.py

--- topic_assignment.py
+++ topic_assignment.py
@@ -1,21 +1,15 @@
 import csv
 
-
 def import_year_topics(fname):
-    # out = []
-    # with open(fname, 'r') as f:
-    #     for e in csv.reader(f):
-    #         topic_data = set(e[1].split())
-    #         topic_num = e[0][-2]
-    #         out.append((topic_num, topic_data))
-    #     return out
-    out = []
+    out = {}
     with open(fname, 'r') as f:
         for e in csv.reader(f):
-            topic_num = e[0]
-            topics = [tuple(element.split(':')) for element in e[1].split()]
-            out.append((topic_num, dict(topics)))
-        return out
+            topic_num = int(e[0])
+            topics = dict([tuple(element.split(':')) for element in e[1].split()])
+            topic_summary = e[2]
+            out[topic_num] = (topics, topic_summary)
+    f.close()
+    return out
 
 def setup(fname):
     out = []
@@ -40,11 +34,11 @@ def calculate_similarity_score(topic, article_topic):
 
 def match(topics, article_topic):
     score_floor = 0.0005
-    l = []
+    l = set()
     for topic in topics:
-        sim_score = calculate_similarity_score(topic[1], article_topic)
+        sim_score = calculate_similarity_score(topics[topic][0], article_topic)
         if sim_score >= score_floor:
-            l.append(topic[0])
+            l.add(topics[topic][1])
     return l
 
         
@@ -54,16 +48,9 @@ if __name__ == '__main__':
     
     for y in range(2020, 2021):
         year = str(y)
-        yearly_topics = import_year_topics('New_NMF_'+year+'.csv')
+        yearly_topics = import_year_topics(year+'_filtered.csv')
         l = []
         for article in setup('Articles_Topics_2020.csv'):
             article_values = match(yearly_topics, article['topic'])
             headline = article['headline']
             print(headline, article_values)
-    # d = {}
-    # for e in l:
-    #     if e[0] not in d:
-    #         d[e[0]] = 0
-    #     d[e[0]]+=1
-    # for k, v in d.items():
-    #     print('topic '+k+': '+str(v)+' entries')
